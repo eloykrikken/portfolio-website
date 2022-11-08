f7923ee596277ba19767767af5f124b3f4540f30
Eloy Krikken <eloykrikken@live.nl>
2022-11-08 08:10:29 +0100

    Some logo changes

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/images/logo.png" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/images/logo.png" width="150" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
